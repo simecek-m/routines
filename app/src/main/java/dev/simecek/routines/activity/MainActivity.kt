20760ef7059f901098d0d231ffd0d588444f0aac
Martin Simecek <simecek.m@seznam.cz>
2021-01-05 23:30:27 +0100

    add Main Activity

---
+++ app/src/main/java/dev/simecek/routines/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.simecek.routines.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.simecek.routines.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
