926f802ffd2d78f78174605cb7416cc5ef4efc7f
Martin Simecek <simecek.m@seznam.cz>
2021-01-06 15:05:22 +0100

    implement navigation component

---
+++ app/src/main/java/dev/simecek/routines/fragment/ListFragment.kt
@@ -0,0 +1,20 @@
+package dev.simecek.routines.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dev.simecek.routines.R
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+
+}
