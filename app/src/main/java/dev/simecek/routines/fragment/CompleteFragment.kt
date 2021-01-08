c02df9d4e5db20b0632aca1a25fd246b79e15ab5
Martin Simecek <simecek.m@seznam.cz>
2021-01-08 13:35:11 +0100

    fix navigation component popUp behavior

--- app/src/main/java/dev/simecek/routines/fragment/CompleteFragment.kt
+++ app/src/main/java/dev/simecek/routines/fragment/CompleteFragment.kt
@@ -5,15 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import dev.simecek.routines.R
+import androidx.navigation.fragment.findNavController
+import dev.simecek.routines.databinding.FragmentCompleteBinding
 
 class CompleteFragment : Fragment() {
+
+    private lateinit var binding: FragmentCompleteBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_complete, container, false)
+        binding = FragmentCompleteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.resume.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 
 }
--- app/src/main/java/dev/simecek/routines/fragment/ListFragment.kt
+++ app/src/main/java/dev/simecek/routines/fragment/ListFragment.kt
@@ -6,21 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import dev.simecek.routines.R
+import dev.simecek.routines.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+
+    private lateinit var binding: FragmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         // TODO check if there are any routines
-        val actionEmptyList = ListFragmentDirections.redirectToComplete()
-        findNavController().navigate(actionEmptyList)
+        binding.list.setOnClickListener {
+            val actionEmptyList = ListFragmentDirections.redirectToEmpty()
+            findNavController().navigate(actionEmptyList)
+        }
     }
 
 }
