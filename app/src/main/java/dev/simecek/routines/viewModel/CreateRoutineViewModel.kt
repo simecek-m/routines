a26c63a4c95119bc29e59542ad501c45960811d0
Martin Simecek <simecek.m@seznam.cz>
2021-01-14 14:54:42 +0100

    use data binding for IconPickerView selectedIcon attribute

--- app/src/main/java/dev/simecek/routines/viewModel/CreateRoutineViewModel.kt
+++ app/src/main/java/dev/simecek/routines/viewModel/CreateRoutineViewModel.kt
@@ -2,11 +2,12 @@ package dev.simecek.routines.viewModel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dev.simecek.routines.constant.IconPickerSelectedType
 
 class CreateRoutineViewModel: ViewModel() {
 
     val title: MutableLiveData<String> = MutableLiveData("")
-    val icon: MutableLiveData<String> = MutableLiveData("ic_loop")
+    val icon: MutableLiveData<IconPickerSelectedType> = MutableLiveData(IconPickerSelectedType.IC_BATH)
     val time: MutableLiveData<String> = MutableLiveData("12:00")
 
 }
