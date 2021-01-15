2adfdd9e5c1f88a10c90d29aa0b01c318ae2e17e
Martin Simecek <simecek.m@seznam.cz>
2021-01-15 17:16:18 +0100

    fix two way data binding for IconPickerView

---
+++ app/src/main/java/dev/simecek/routines/listener/IconPickerListener.kt
@@ -0,0 +1,5 @@
+package dev.simecek.routines.listener
+
+interface IconPickerListener {
+    fun onSelectedIconChanged()
+}
--- app/src/main/java/dev/simecek/routines/views/IconPickerView.kt
+++ app/src/main/java/dev/simecek/routines/views/IconPickerView.kt
@@ -5,23 +5,29 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.BindingAdapter
+import androidx.databinding.InverseBindingAdapter
+import androidx.databinding.InverseBindingListener
 import com.airbnb.paris.extensions.style
 import dev.simecek.routines.R
 import dev.simecek.routines.constant.IconPickerSelectedType
 import dev.simecek.routines.databinding.ViewIconPickerBinding
+import dev.simecek.routines.listener.IconPickerListener
 
 class IconPickerView(context: Context, attrs: AttributeSet?) : ConstraintLayout(context, attrs) {
 
     private var binding: ViewIconPickerBinding = ViewIconPickerBinding.inflate(LayoutInflater.from(context), this, true)
     private var selectedIcon: Int = 0
+    var listener: IconPickerListener? = null
 
-    fun getSelectedIcon():String {
-        return findIconView(selectedIcon)?.tag.toString()
+    fun getSelectedIcon():IconPickerSelectedType {
+        return IconPickerSelectedType.values()[selectedIcon]
     }
 
     fun setSelectedIcon(icon: IconPickerSelectedType) {
         refreshUI(selectedIcon, icon.ordinal)
         selectedIcon = icon.ordinal
+        listener?.onSelectedIconChanged()
     }
 
     init {
@@ -38,7 +44,7 @@ class IconPickerView(context: Context, attrs: AttributeSet?) : ConstraintLayout(
     }
 
     private fun findIconView(iconTypeNumber: Int?): ImageView? {
-        return when(iconTypeNumber) {
+        return when (iconTypeNumber) {
             0 -> binding.iconLoop
             1 -> binding.iconSchool
             2 -> binding.iconWalk
@@ -55,6 +61,32 @@ class IconPickerView(context: Context, attrs: AttributeSet?) : ConstraintLayout(
             13 -> binding.iconBeer
             14 -> binding.iconChat
             else -> null
+        }
+    }
+
+    companion object {
+        @BindingAdapter("app:selectedIconAttrChanged")
+        @JvmStatic
+        fun setListeners(view: IconPickerView, attrChange: InverseBindingListener) {
+            view.listener = object: IconPickerListener {
+                override fun onSelectedIconChanged() {
+                    attrChange.onChange()
+                }
+            }
+        }
+
+        @JvmStatic
+        @InverseBindingAdapter(attribute = "app:selectedIcon")
+        fun getSelectedIcon(view: IconPickerView): IconPickerSelectedType {
+            return view.getSelectedIcon()
+        }
+
+        @JvmStatic
+        @BindingAdapter("app:selectedIcon")
+        fun setSelectedIcon(view: IconPickerView, icon: IconPickerSelectedType) {
+            if(view.selectedIcon != icon.ordinal) {
+                view.setSelectedIcon(icon)
+            }
         }
     }
 
