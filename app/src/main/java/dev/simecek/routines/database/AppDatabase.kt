189e9a13fc73028236fa60913a62f952cceb44c5
Martin Simecek <simecek.m@seznam.cz>
2021-01-17 10:53:40 +0100

    implement singleton Database instance

--- app/src/main/java/dev/simecek/routines/database/AppDatabase.kt
+++ app/src/main/java/dev/simecek/routines/database/AppDatabase.kt
@@ -1,6 +1,8 @@
 package dev.simecek.routines.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import dev.simecek.routines.converter.IconPickerConverter
@@ -11,4 +13,22 @@ import dev.simecek.routines.model.Routine
 @TypeConverters(IconPickerConverter::class)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun routineDao(): RoutineDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "app-database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
