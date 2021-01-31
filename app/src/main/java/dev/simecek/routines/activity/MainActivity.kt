17e97eb7cf753815afdcaf2cbd25e832a64f2fbd
Martin Simecek <simecek.m@seznam.cz>
2021-01-31 13:45:27 +0100

    implement "transparent" status and navigation bars

--- app/src/main/java/dev/simecek/routines/activity/MainActivity.kt
+++ app/src/main/java/dev/simecek/routines/activity/MainActivity.kt
@@ -1,7 +1,9 @@
 package dev.simecek.routines.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import dev.simecek.routines.R
 
@@ -10,5 +12,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setBlackStatusAndNavigationBars()
+    }
+
+    private fun setBlackStatusAndNavigationBars() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            window.statusBarColor = Color.BLACK
+        }
+
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.O_MR1) {
+            window.navigationBarColor = Color.BLACK
+        }
     }
 }
