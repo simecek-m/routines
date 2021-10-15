ad7cf0143273d52fd26dc054be1d5838974f025d
Martin Šimeček <simecek.m@seznam.cz>
2021-10-15 18:58:50 +0200

    add basic layout for Account Picker Fragment

---
+++ app/src/main/java/dev/simecek/routines/fragment/AccountPickerFragment.kt
@@ -0,0 +1,19 @@
+package dev.simecek.routines.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dev.simecek.routines.R
+
+class AccountPickerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_account_picker, container, false)
+    }
+
+}
