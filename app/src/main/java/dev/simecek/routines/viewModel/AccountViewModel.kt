83d3fbcc6e4154fa3b15b451c8b3c7670a31f8e5
Martin Šimeček <simecek.m@seznam.cz>
2021-11-01 18:52:33 +0100

    clear alarms after sign out

--- app/src/main/java/dev/simecek/routines/viewModel/AccountViewModel.kt
+++ app/src/main/java/dev/simecek/routines/viewModel/AccountViewModel.kt
@@ -4,19 +4,26 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.simecek.routines.database.entity.Routine
 import dev.simecek.routines.database.entity.User
+import dev.simecek.routines.database.repository.RoutineRepository
 import dev.simecek.routines.database.repository.UserRepository
 import javax.inject.Inject
 
 @HiltViewModel
 class AccountViewModel @Inject constructor(
-    val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val routineRepository: RoutineRepository
 ): ViewModel() {
 
     var user: LiveData<User> = MutableLiveData(User("", ""))
 
     fun getUser(userName: String) {
         user = userRepository.getUserByName(userName)
+    }
+
+    suspend fun getAllActiveRoutines(): List<Routine> {
+        return routineRepository.getAllRoutinesAsList()
     }
 
     suspend fun deleteAccount() {
